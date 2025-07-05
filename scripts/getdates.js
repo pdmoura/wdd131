707c067fcfa9aa2086baf161811d98b86b686627
pdmoura <sudwarriormoura@gmail.com>
2025-07-05 15:28:50 -0300

    Initial Layout of the index page in pt-BR

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const today = new Date();
+const copyYear = document.getElementById("currentyear");
+const modifiedDate = document.getElementById("lastModified")
+document.addEventListener('DOMContentLoaded', 
+    function() {
+        const copyYear = document.getElementById("currentyear");
+        copyYear.textContent = today.getFullYear();
+        modifiedDate.textContent = "Last Modification: " + document.lastModified;
+    }
+);
+updateTime();
+setInterval(updateTime, 1000);
