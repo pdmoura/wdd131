8d0c706f73eedcc7ec1dd784673e99e422c7298b
pdmoura <sudwarriormoura@gmail.com>
2025-07-06 01:37:58 -0300

    Last modifications + media query added for responsive design

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,7 +3,6 @@ const copyYear = document.getElementById("currentyear");
 const modifiedDate = document.getElementById("lastModified")
 document.addEventListener('DOMContentLoaded', 
     function() {
-        const copyYear = document.getElementById("currentyear");
         copyYear.textContent = today.getFullYear();
         modifiedDate.textContent = "Last Modification: " + document.lastModified;
     }
