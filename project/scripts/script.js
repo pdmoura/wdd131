2105f6c6ede529589b52537d5a6259beaf4829dc
pdmoura <sudwarriormoura@gmail.com>
2025-07-31 17:57:20 -0300

    A few adjustments on the main page, added a svg icon on the completed tasks and created the site plan inicial layout

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,12 +1,12 @@
-    const today = new Date();
-    const copyYear = document.getElementById("currentyear");
-    const modifiedDate = document.getElementById("lastModified")
-    document.addEventListener('DOMContentLoaded',
-        function () {
-            copyYear.textContent = today.getFullYear();
-            modifiedDate.textContent = "Last Modification: " + document.lastModified;
-        }
-    );
+const today = new Date();
+const copyYear = document.getElementById("currentyear");
+const modifiedDate = document.getElementById("lastModified")
+document.addEventListener('DOMContentLoaded',
+    function () {
+        copyYear.textContent = today.getFullYear();
+        modifiedDate.textContent = "Last Modification: " + document.lastModified;
+    }
+);
 
 document.addEventListener('DOMContentLoaded', () => {
     const newTaskInput = document.getElementById('new-task-input');
@@ -39,16 +39,21 @@ document.addEventListener('DOMContentLoaded', () => {
             li.setAttribute('data-index', index);
             li.className = task.completed ? 'completed' : ''; // Add 'completed' class if task is done
 
-            li.innerHTML = `
+            li.innerHTML = ` 
                 <span class="task-text">${task.text}</span>
                 <div class="button-group">
-                    <button class="complete-button">${task.completed ? 'Uncomplete' : 'Complete'}</button>
+                    <button class="complete-button" aria-label="Complete">${task.completed ? 'Uncomplete' : 'Complete'}</button>
                     <button class="delete-button">Delete</button>
                 </div>
             `;
             taskList.appendChild(li);
         });
     };
+
+
+
+
+
 
     // Function to render completed tasks on the Completed Tasks page
     const renderCompletedTasks = () => {
@@ -59,6 +64,9 @@ document.addEventListener('DOMContentLoaded', () => {
             const li = document.createElement('li');
             li.setAttribute('data-index', index);
             li.innerHTML = `
+                <span class="task-text completed-task-text" id="check-icon"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
+<linearGradient id="1ayUTr30BaMDjOG69N2fSa_xTkoPEFGI0P7_gr1" x1="21.241" x2="3.541" y1="39.241" y2="21.541" gradientUnits="userSpaceOnUse"><stop offset=".108" stop-color="#0d7044"></stop><stop offset=".433" stop-color="#11945a"></stop></linearGradient><path fill="url(#1ayUTr30BaMDjOG69N2fSa_xTkoPEFGI0P7_gr1)" d="M16.599,41.42L1.58,26.401c-0.774-0.774-0.774-2.028,0-2.802l4.019-4.019	c0.774-0.774,2.028-0.774,2.802,0L23.42,34.599c0.774,0.774,0.774,2.028,0,2.802l-4.019,4.019	C18.627,42.193,17.373,42.193,16.599,41.42z"></path><linearGradient id="1ayUTr30BaMDjOG69N2fSb_xTkoPEFGI0P7_gr2" x1="-15.77" x2="26.403" y1="43.228" y2="43.228" gradientTransform="rotate(134.999 21.287 38.873)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2ac782"></stop><stop offset="1" stop-color="#21b876"></stop></linearGradient><path fill="url(#1ayUTr30BaMDjOG69N2fSb_xTkoPEFGI0P7_gr2)" d="M12.58,34.599L39.599,7.58c0.774-0.774,2.028-0.774,2.802,0l4.019,4.019	c0.774,0.774,0.774,2.028,0,2.802L19.401,41.42c-0.774,0.774-2.028,0.774-2.802,0l-4.019-4.019	C11.807,36.627,11.807,35.373,12.58,34.599z"></path>
+</svg></span>
                 <span class="task-text" id=dashed-text>${task.text}</span>
                 <button class="delete-completed-button">Delete</button>
             `;
