b676f9200513f42f05ebc3d35d3c94db66c07654
pdmoura <sudwarriormoura@gmail.com>
2025-08-03 03:25:10 -0300

    Finished the website with all the functionalities

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,31 @@
 const products = [
     {
-      id: "fc-1888",
-      name: "My To-Do List App",
-      averagerating: 4.5
+        id: "fc-1888",
+        name: "My To-Do List App",
+        averagerating: 5
+    },
+    {
+        id: "fc-1889",
+        name: "Add task Functionality",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-1890",
+        name: "Task List Vizualization",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-1891",
+        name: "Task Completion Feature",
+        averagerating: 4.0
     }
 ];
 
 const today = new Date();
 const copyYear = document.getElementById("currentyear");
 const modifiedDate = document.getElementById("lastModified")
-document.addEventListener('DOMContentLoaded', 
-    function() {
+document.addEventListener('DOMContentLoaded',
+    function () {
         const copyYear = document.getElementById("currentyear");
         copyYear.textContent = today.getFullYear();
         modifiedDate.textContent = "Last Modification: " + document.lastModified;
@@ -23,7 +38,7 @@ document.addEventListener('DOMContentLoaded',
     }
 );
 
-document.addEventListener('submit', function() {
+document.addEventListener('submit', function () {
     let submittedReviews = localStorage.getItem('submittedReviews');
     if (submittedReviews) {
         submittedReviews = Number(submittedReviews) + 1;
